2203173703827378333f6b90598c2fbca49c5854
Wendy Bartos <54962661+WEB818@users.noreply.github.com>
2020-03-23 17:15:11 -0700

    mobile styles

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-const toggleMenu = function() {
-  $('.menu-dropdown').click(() => {
-    $('ul').toggleClass('active');
+/* eslint-disable quotes */
+
+const navSlide = () => {
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  $(".burger").click(() => {
+    //Toggle Nav
+    $(".nav-links").toggleClass("nav-active");
+    //Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 +
+          0.5}s`;
+      }
+    });
+    //Burger animation
+    $(".burger").toggleClass("bar");
   });
 };
-  
-const main = function() {
-  toggleMenu();
+
+const viewMore = () => {
+  $(".content").click(() => {
+    $(".read-more").toggleClass("view-active");
+  });
 };
-  
+const viewLess = () => {
+  $(".read-less").click(() => {
+    $(".read-less").toggleClass("read-more");
+  });
+};
+const main = function() {
+  navSlide();
+  viewMore();
+  viewLess();
+};
+
 $(main);
-  
-  
