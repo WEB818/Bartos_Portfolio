e9b85a586d02e03ed3a66cee68276250655d87d1
Wendy Bartos <54962661+WEB818@users.noreply.github.com>
2020-03-23 20:25:03 -0700

    fixed nav overflow

--- index.js
+++ index.js
@@ -20,20 +20,26 @@ const navSlide = () => {
   });
 };
 
-const viewMore = () => {
-  $(".content").click(() => {
-    $(".read-more").toggleClass("view-active");
+const hideMenu = () => {
+  const navLinks = document.querySelectorAll(".nav-links li");
+  $("li").click(() => {
+    $(".nav-links").toggleClass("nav-active");
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 +
+          0.5}s`;
+      }
+    });
+    //Burger animation
+    $(".burger").toggleClass("bar");
   });
 };
-const viewLess = () => {
-  $(".read-less").click(() => {
-    $(".read-less").toggleClass("read-more");
-  });
-};
+
 const main = function() {
   navSlide();
-  viewMore();
-  viewLess();
+  hideMenu();
 };
 
 $(main);
