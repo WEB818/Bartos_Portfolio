37407a717c98a370538ffc1a55b479c86c1866ea
Wendy Bartos <54962661+WEB818@users.noreply.github.com>
2021-03-29 13:52:41 -0700

    added contact form

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /* eslint-disable quotes */
+// import axios from "./axios.js";
 
 const navSlide = () => {
   const navLinks = document.querySelectorAll(".nav-links li");
@@ -37,9 +38,50 @@ const hideMenu = () => {
   });
 };
 
+const sendEmail = () => {
+  $(".contact-form").submit((ev) => {
+    ev.preventDefault();
+    let name = ev.target.name.value;
+    let email = ev.target.email.value;
+    let content = ev.target.content.value;
+
+    $(".form-container").toggleClass("submitted");
+    $(".contact-form").toggleClass("submitted");
+    $(".message-header").toggleClass("submitted");
+    $(".reply-container").toggleClass("thank-you-msg");
+
+    fetch(
+      "https://yfxth9xlwl.execute-api.us-west-2.amazonaws.com/prod/email/send",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          name: name,
+          email: email,
+          content: content,
+        }),
+      }
+    )
+      .then((res) => res.json())
+      .catch((err) => console.error(err));
+    ev.target.name.value = "";
+    ev.target.email.value = "";
+    ev.target.content.value = "";
+  });
+};
+
+const closeMsg = () => {
+  $(".close-msg").click(() => {
+    $(".form-container").toggleClass("submitted");
+    $(".contact-form").toggleClass("submitted");
+    $(".message-header").toggleClass("submitted");
+    $(".reply-container").toggleClass("thank-you-msg");
+  });
+};
 const main = function () {
   navSlide();
   hideMenu();
+  sendEmail();
+  closeMsg();
 };
 
 $(main);
